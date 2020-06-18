7b6344029c6f5b70f701913a7d7ccb394d0c358c
Arthur M. Vilela <av@norr.group>
2020-06-18 13:48:05 -0300

    Criado ServidorAPI, estrutura básica

---
+++ jorgesCarros/src/main/java/API/ServidorAPI.java
@@ -0,0 +1,23 @@
+package API;
+
+import com.sun.net.httpserver.HttpServer;
+import java.net.InetSocketAddress;
+
+/**
+ * Está classes roda um servidor uma API REST que serve os
+ * recursos do sistemas por HTTP 
+ */
+public class ServidorAPI {
+    public static void main(String[] args){
+        try {
+            //cria o servidor e atribui a ele a porta 8080 do localhost
+            HttpServer servidor = HttpServer.create(new InetSocketAddress("localhost", 8080), 0);
+                        
+            //começa o servidor
+            servidor.setExecutor(null); 
+            servidor.start();
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+}
