ebbd8a19c8db4e82e80b7591684e41996e493312
Arthur M. Vilela <av@norr.group>
2020-06-30 16:00:49 -0300

    Criado classes Cliente e Endereco

---
+++ jorgesCarros/src/main/java/Cliente/Cliente.java
@@ -0,0 +1,65 @@
+package Cliente;
+
+import java.time.LocalDate;
+
+/**
+ * Representa os dados de um cliente no sistema.
+ */
+public class Cliente {
+    private String rg, cpf, nome, cnh;
+    private LocalDate dataNascimento;
+    private Endereco endereco;
+
+    public Cliente() {
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCnh() {
+        return cnh;
+    }
+
+    public void setCnh(String cnh) {
+        this.cnh = cnh;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+    
+    
+}
