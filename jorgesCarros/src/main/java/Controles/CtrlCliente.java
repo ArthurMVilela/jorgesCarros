3c5a20bf0ab98578ac394ba3e3cf91ea4b9bce2d
Arthur M. Vilela <av@norr.group>
2020-07-03 13:38:02 -0300

    criado front

---
+++ jorgesCarros/src/main/java/Controles/CtrlCliente.java
@@ -0,0 +1,10 @@
+package Controles;
+
+/**
+ * Classe controle para todas as operações envolvendo o 
+ * pacote Cliente e persitencia de dados de entidades do
+ * pacote Cliente.
+ */
+public class CtrlCliente {
+    
+}
