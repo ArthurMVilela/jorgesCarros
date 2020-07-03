6a5ccb2bff928670b604ed7e1c0a75f1d1009d20
Arthur M. Vilela <av@norr.group>
2020-07-03 14:25:28 -0300

    Adicionado menu e tela de cadastro de carros, precisa melhorar e implmentar API

--- front/index.js
+++ front/index.js
@@ -1,10 +1,19 @@
 var express = require("express"); 
+const path = require('path');
 
 var app = express();
 
-app.get("/", function(req,res) {
-	res.send("Hello world");
+// Setup view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get("/", function(req,res,next) {
+	res.render("index", {});
 });
+
+app.get("/cadastrar-carro", function(req,res,next) {
+	res.render("carros/cadastrar-carros", {})
+})
 
 
 var server = app.listen(3000, function(){});
